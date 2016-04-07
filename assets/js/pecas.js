1085df6a57207352ef5b5078c50f3e8dc8d69549
Leandro Cura <leandrocura@gmail.com>
2016-04-07 14:11:22 -0300

    Version 1 de Hyped

---
+++ assets/js/pecas.js
@@ -0,0 +1,130 @@
+$.scrollTo = $.fn.scrollTo = function(x, y, options){
+    if (!(this instanceof $)) return $.fn.scrollTo.apply($('html, body'), arguments);
+
+    options = $.extend({}, {
+        gap: {
+            x: 0,
+            y: 0
+        },
+        animation: {
+            easing: 'swing',
+            duration: 600,
+            complete: $.noop,
+            step: $.noop
+        }
+    }, options);
+
+    return this.each(function(){
+        var elem = $(this);
+        elem.stop().animate({
+            scrollLeft: !isNaN(Number(x)) ? x : $(y).offset().left + options.gap.x,
+            scrollTop: !isNaN(Number(y)) ? y : $(y).offset().top + options.gap.y
+        }, options.animation);
+    });
+};
+
+
+$(document).ready(function(){
+
+        $(window).scroll(function(){
+            var window_top = $(window).scrollTop() + 12; // the "12" should equal the margin-top value for nav.stick
+            var div_top = $('#nav-anchor').offset().top;
+                if (window_top > div_top) {
+                    $('.menu nav').addClass('stick');
+                } else {
+                    $('.menu nav').removeClass('stick');
+                }
+        });
+
+        $(".menu nav a").click(function(evn){
+            evn.preventDefault();
+            $('html,body').scrollTo(this.hash, this.hash); 
+        });
+        
+        var aChildren = $(".menu nav li").children(); // find the a children of the list items
+        var aArray = []; // create the empty aArray
+        for (var i=0; i < aChildren.length; i++) {    
+            var aChild = aChildren[i];
+            var ahref = $(aChild).attr('href');
+            aArray.push(ahref);
+        } // this for loop fills the aArray with attribute href values
+        
+        $(window).scroll(function(){
+            var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+            var windowHeight = $(window).height(); // get the height of the window
+            var docHeight = $(document).height();
+            
+            for (var i=0; i < aArray.length; i++) {
+                var theID = aArray[i];
+                var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+                var divHeight = $(theID).height(); // get the height of the div in question
+                if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                    $("a[href='" + theID + "']").addClass("active");
+                } else {
+                    $("a[href='" + theID + "']").removeClass("active");
+                }
+            }
+
+        });
+    });
+//Code stolen from css-tricks for smooth scrolling:
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+function showDate()
+{
+	var now = new Date();
+	var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+	var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+	var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+	function fourdigits(number)
+	{
+		return (number < 1000) ? number + 1900 : number;
+	}
+	
+	tnow=new Date();
+	thour=now.getHours();
+	tmin=now.getMinutes();
+	tsec=now.getSeconds();
+	
+	if (tmin<=9) { tmin="0"+tmin; }
+	if (tsec<=9) { tsec="0"+tsec; }
+	if (thour<10) { thour="0"+thour; }
+	
+	today = days[now.getDay()] + ", " + date + " " + months[now.getMonth()] + ", " + (fourdigits(now.getYear())) + " - " + thour + ":" + tmin +":"+ tsec;
+	document.getElementById("dateDiv").innerHTML = today;
+}
+setInterval("showDate()", 1000);
+
+$(function() {
+    
+    var contador = 1;     
+    $('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+
+		if(contador == 1){
+            contador = 0;
+			$('nav').animate({ right: '0'});
+            $('.bt-menu').addClass("active");
+		} else {
+			contador = 1;
+			$('nav').animate({
+				right: '-250px'
+			});
+            $('.bt-menu').removeClass("active");
+		}
+
+	}); 
+});
+                    
